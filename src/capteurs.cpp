d83f2b4be0ba23061599477b60b711d7a6784267
Clément_R <rinnclement@gmail.com>
2024-10-23 09:54:36 +0200

    modification capteur gps

--- src/capteurs.cpp
+++ src/capteurs.cpp
@@ -1,11 +1,22 @@
 #include "variables.h"
 #include <SoftwareSerial.h>
 
+unsigned char buffer[64];
+int count=0;
+
 void acquerirDonnees() {
   // Acquérir les données
 }
 
   // Acquisition données capteur
+
+void clearBufferArray()                     // function to clear buffer array
+{
+  for (int i=0; i<count;i++)
+  {
+    buffer[i]=NULL;
+  }                      // clear all index of array with command NULL
+}
 
   void acquisition_donnee_capteurs(){
   SoftwareSerial SoftSerial(2, 3);
@@ -20,7 +31,7 @@ void acquerirDonnees() {
       buffer[count++]=SoftSerial.read();      // writing data into array
       if(count == 64)break;
     }
-    Serial.write(buffer,count);                 // if no data transmission ends, write buffer to hardware serial port
+    Serial.write(reinterpret_cast<const char *>(buffer), count);                 // if no data transmission ends, write buffer to hardware serial port
     clearBufferArray();                         // call clearBufferArray function to clear the stored data from the array
     count = 0;                                  // set counter of while loop to zero
   }
@@ -29,13 +40,7 @@ void acquerirDonnees() {
 }
 
 
-void clearBufferArray()                     // function to clear buffer array
-{
-  for (int i=0; i<count;i++)
-  {
-    buffer[i]=NULL;
-  }                      // clear all index of array with command NULL
-}
+
 
 
 void sauvegarderDonnees(const Mode& currentMode) {
