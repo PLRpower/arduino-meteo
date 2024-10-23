05048962af5257270386dc15b0b0a9ce48fb8955
Clément_R <rinnclement@gmail.com>
2024-10-23 09:27:32 +0200

    Ajout du code (non finalisé) pour faire fonctionner le gps.

--- src/capteurs.cpp
+++ src/capteurs.cpp
@@ -1,8 +1,42 @@
 #include "variables.h"
+#include <SoftwareSerial.h>
 
 void acquerirDonnees() {
   // Acquérir les données
 }
+
+  // Acquisition données capteur
+
+  void acquisition_donnee_capteurs(){
+  SoftwareSerial SoftSerial(2, 3);
+  unsigned char buffer[64];                   // buffer array for data receive over serial port
+  int count=0;                                // counter for buffer array
+
+
+  if (SoftSerial.available())                     // if date is coming from software serial port ==> data is coming from SoftSerial shield
+  {
+    while(SoftSerial.available())               // reading data into char array
+    {
+      buffer[count++]=SoftSerial.read();      // writing data into array
+      if(count == 64)break;
+    }
+    Serial.write(buffer,count);                 // if no data transmission ends, write buffer to hardware serial port
+    clearBufferArray();                         // call clearBufferArray function to clear the stored data from the array
+    count = 0;                                  // set counter of while loop to zero
+  }
+  if (Serial.available())                 // if data is available on hardware serial port ==> data is coming from PC or notebook
+    SoftSerial.write(Serial.read());        // write it to the SoftSerial shield
+}
+
+
+void clearBufferArray()                     // function to clear buffer array
+{
+  for (int i=0; i<count;i++)
+  {
+    buffer[i]=NULL;
+  }                      // clear all index of array with command NULL
+}
+
 
 void sauvegarderDonnees(const Mode& currentMode) {
   if(currentMode != MODE_MAINTENANCE) {
