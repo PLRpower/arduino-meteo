9dc20f26559aeaca5567a268a792b1d4a786b8b5
plr <paul.thomas.stras@gmail.com>
2024-10-23 14:51:20 +0200

    Correction affichage

--- src/capteurs.cpp
+++ src/capteurs.cpp
@@ -3,59 +3,45 @@
 
 unsigned char buffer[64];
 int count=0;
-const int lightPin = A0;
 int sensorValue = 0;
 
 void acquerirDonnees() {
-  // Acquérir les données
+    int lightData = analogRead(A0);
 }
 
-  // Acquisition données capteur
 
-void clearBufferArray()                     // function to clear buffer array
-{
-  for (int i=0; i<count;i++)
-  {
-    buffer[i]=NULL;
-  }                      // clear all index of array with command NULL
+void clearBufferArray() {
+    for (int i=0; i<count;i++) {
+        buffer[i]=NULL;
+    }
 }
 
 void acquisition_donnee_gps() {
-  SoftwareSerial SoftSerial(2, 3);
-  unsigned char buffer[64];                   // buffer array for data receive over serial port
-  int count=0;                                // counter for buffer array
+    SoftwareSerial SoftSerial(2, 3);
+    unsigned char buffer[64];
+    int count=0;
 
-
-  if (SoftSerial.available())                     // if date is coming from software serial port ==> data is coming from SoftSerial shield
-  {
-    while(SoftSerial.available())               // reading data into char array
-    {
-      buffer[count++]=SoftSerial.read();      // writing data into array
-      if(count == 64)break;
+    if (SoftSerial.available()) {
+        while(SoftSerial.available()) {
+            buffer[count++]=SoftSerial.read();
+            if(count == 64)break;
+        }
+        Serial.write(reinterpret_cast<const char *>(buffer), count);
+        clearBufferArray();
+        count = 0;
     }
-    Serial.write(reinterpret_cast<const char *>(buffer), count);                 // if no data transmission ends, write buffer to hardware serial port
-    clearBufferArray();                         // call clearBufferArray function to clear the stored data from the array
-    count = 0;                                  // set counter of while loop to zero
-  }
-  if (Serial.available())                 // if data is available on hardware serial port ==> data is coming from PC or notebook
-  {
-    SoftSerial.write(Serial.read());        // write it to the SoftSerial shield
-  }
-}
 
-void acquisition_donnee_light() {
-  analogRead(lightPin);
+    if (Serial.available()) {
+        SoftSerial.write(Serial.read());
+    }
 }
 
 void acquisition_donnee_temperature() {
 
 }
 
-
-
-
 void sauvegarderDonnees(const Mode& currentMode) {
-  if(currentMode != MODE_MAINTENANCE) {
-    // Sauvegarde des données
-  }
+    if(currentMode != MODE_MAINTENANCE) {
+        // Sauvegarde des données
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,48 +9,6 @@ Mode currentMode;
 Config config;
 DS1307 clock;
 
-void printTime()
-{
-    clock.getTime();
-    Serial.print(clock.hour, DEC);
-    Serial.print(":");
-    Serial.print(clock.minute, DEC);
-    Serial.print(":");
-    Serial.print(clock.second, DEC);
-    Serial.print("  ");
-    Serial.print(clock.dayOfMonth, DEC);
-    Serial.print("/");
-    Serial.print(clock.month, DEC);
-    Serial.print("/");
-    Serial.print(clock.year + 2000, DEC);
-    Serial.print(" ");
-    switch (clock.dayOfWeek)
-    {
-        case MON:
-            Serial.print("LUN");
-        break;
-        case TUE:
-            Serial.print("MAR");
-        break;
-        case WED:
-            Serial.print("MER");
-        break;
-        case THU:
-            Serial.print("JEU");
-        break;
-        case FRI:
-            Serial.print("VEN");
-        break;
-        case SAT:
-            Serial.print("SAM");
-        break;
-        case SUN:
-            Serial.print("DIM");
-        break;
-    }
-    Serial.println(" ");
-}
-
 void setup() {
     Serial.begin(9600);
     led.init();
@@ -58,16 +16,14 @@ void setup() {
     pinMode(greenButton.pin, INPUT_PULLUP);
     clock.begin();
 
-    if(demarrage(redButton, led)) {  // Si le bouton rouge est appuyé au démarrage
-        setModeConfig(led, config, clock);  // Mode configuration
+    if(demarrage(redButton, led)) {
+        setModeConfig(led, config, clock);
     }
 
-    setModeStandard(led, currentMode);  // Mode standard si le bouton rouge n'est pas appuyé ou après 30 secondes d'inactivité
+    setModeStandard(led, currentMode);
 }
 
 void loop() {
-    printTime();
-
     handleButton(redButton);
     handleButton(greenButton);
 
